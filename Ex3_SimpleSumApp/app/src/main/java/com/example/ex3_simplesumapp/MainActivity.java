d5213c6e446404ee3c72ab378c41a33bb229f48d
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-03-13 08:43:54 +0700

    TH tại nhà

--- Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // gắn Layout tương ứng với file này
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+    // đây là bộ lắng nghe và xử lí sự kiện click lên nút "Tính Tổng"
+    public void XuLyCong(View view){
+    // tìm, tham chiếu đến điều khiển trên tệp XML, maping sang java file
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+    //lấy dữ liệu về ở điều khiển số A
+        String strA = editTextSoA.getText().toString();
+    //lấy dữ liệu về ở điều khiển số A
+        String strB = editTextSoB.getText().toString();
+    // chuyển dữ liệu sang dạng số
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+    // tính toán theo yêu cầu
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong); // chuyển sang dạng chuỗi
+    // hiện ra màn hình
+    editTextKetQua.setText(strTong);
+
+    }
+
+
 }
