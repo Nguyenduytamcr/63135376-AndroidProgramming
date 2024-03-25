5298dd49da40df573b38f80d49e634d853bb829a
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-03-25 19:33:13 +0700

    .

---
+++ Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.cau2_appbmi;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnDanhGia;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText edtCanNang = findViewById(R.id.edtCanNang);
+        EditText edtChieuCao = 
+
+    }
+}
