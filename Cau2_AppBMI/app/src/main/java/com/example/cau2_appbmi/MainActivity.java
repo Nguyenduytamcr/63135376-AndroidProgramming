e2944093844a1f6099829ea7361485a84372a954
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-03-25 19:36:57 +0700

    up

--- Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cau2_appbmi;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
         EditText edtChieuCao = findViewById(R.id.edtChieuCao);
         Button btnDanhGia = findViewById(R.id.btnDanhGia);
 
+        btnDanhGia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double w = Double.parseDouble(edtCanNang.getText().toString());
+                double h = Double.parseDouble(edtChieuCao.getText().toString());
+            }
+        });
     }
 }
