33ca264398ce431ebda637c9a7a0215bb560357e
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-03-25 20:51:09 +0700

    làm lại từ đầu

--- Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
@@ -19,16 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edtCanNang = findViewById(R.id.edtCanNang);
-        EditText edtChieuCao = findViewById(R.id.edtChieuCao);
-        Button btnDanhGia = findViewById(R.id.btnDanhGia);
 
-        btnDanhGia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-            }
-        });
     }
 }
