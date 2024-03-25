9d5739d86dfd1c9c74379c0cc998cebdb59e45bd
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-03-25 21:45:00 +0700

    up

--- Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
@@ -42,27 +42,57 @@ public class MainActivity extends AppCompatActivity {
                     }
                     else if(chiso >= 18.5 && chiso <= 24.9){
                         txtKetQua.setText(String.valueOf(chiso));
-                        txtNhanXet.setText("Bạn có chỉ số");
+                        txtNhanXet.setText("Bạn có chỉ số BMI bình thường");
                     }
                     else if(chiso == 25) {
                         txtKetQua.setText(String.valueOf(chiso));
-                        txtNhanXet.setText("Bạn cần bồi bổ thêm!");
+                        txtNhanXet.setText("Bạn đang thừa cân!");
                     }
                     else if(chiso > 25 && chiso <= 29.9){
                         txtKetQua.setText(String.valueOf(chiso));
-                        txtNhanXet.setText("Bạn cần bồi bổ thêm!");
+                        txtNhanXet.setText("Bạn đang ở giai đoạn tiền béo phì/ béo phì mức độ thấp!");
                     }
                     else if(chiso >= 30 && chiso <= 34.9){
                         txtKetQua.setText(String.valueOf(chiso));
-                        txtNhanXet.setText("Bạn cần bồi bổ thêm!");
+                        txtNhanXet.setText("Bạn đang ở béo phì độ I");
                     }
                     else if(chiso >=35 && chiso <= 39.9){
                         txtKetQua.setText(String.valueOf(chiso));
-                        txtNhanXet.setText("Bạn cần bồi bổ thêm!");
+                        txtNhanXet.setText("Bạn đang béo phì độ II");
                     }
                     else if(chiso == 40){
                         txtKetQua.setText(String.valueOf(chiso));
+                        txtNhanXet.setText("Bạn đang béo phì độ III");
+                    }
+                }
+                else if(rbnu.isChecked()){
+                    if(chiso < 18.5){
+                        txtKetQua.setText(String.valueOf(chiso));
                         txtNhanXet.setText("Bạn cần bồi bổ thêm!");
+                    }
+                    else if(chiso >= 18.5 && chiso <= 24.9){
+                        txtKetQua.setText(String.valueOf(chiso));
+                        txtNhanXet.setText("Bạn có chỉ số BMI bình thường");
+                    }
+                    else if(chiso == 25) {
+                        txtKetQua.setText(String.valueOf(chiso));
+                        txtNhanXet.setText("Bạn đang thừa cân!");
+                    }
+                    else if(chiso > 25 && chiso <= 29.9){
+                        txtKetQua.setText(String.valueOf(chiso));
+                        txtNhanXet.setText("Bạn đang ở giai đoạn tiền béo phì/ béo phì mức độ thấp!");
+                    }
+                    else if(chiso >= 30 && chiso <= 34.9){
+                        txtKetQua.setText(String.valueOf(chiso));
+                        txtNhanXet.setText("Bạn đang ở béo phì độ I");
+                    }
+                    else if(chiso >=35 && chiso <= 39.9){
+                        txtKetQua.setText(String.valueOf(chiso));
+                        txtNhanXet.setText("Bạn đang béo phì độ II");
+                    }
+                    else if(chiso == 40){
+                        txtKetQua.setText(String.valueOf(chiso));
+                        txtNhanXet.setText("Bạn đang béo phì độ III");
                     }
                 }
             }
