84bbb925de4823f35730ff7d2c63386bd4ab2c1c
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-03-25 21:57:35 +0700

    up

--- Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/com/example/cau2_appbmi/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.cau2_appbmi;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +8,7 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
