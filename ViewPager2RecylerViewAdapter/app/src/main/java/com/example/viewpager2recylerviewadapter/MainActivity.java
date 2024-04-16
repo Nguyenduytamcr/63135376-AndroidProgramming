c98ba88c7cd1c344982f3c01bbdaaafbe6956147
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-04-16 07:11:06 +0700

    Bai Hoc Hom Nay
    up ++++

--- ViewPager2RecylerViewAdapter/app/src/main/java/com/example/viewpager2recylerviewadapter/MainActivity.java
+++ ViewPager2RecylerViewAdapter/app/src/main/java/com/example/viewpager2recylerviewadapter/MainActivity.java
@@ -23,7 +23,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         viewPagerDatas = new getDataForViewPager();
         viewPager2Land = findViewById(R.id.vp2Land);
-        landScapeAdapter = new LandScapeAdapter(this,viewPagerDatas,)
+        landScapeAdapter = new LandScapeAdapter(this,viewPagerDatas);
+        viewPager2Land.setAdapter(landScapeAdapter);
+        viewPager2Land.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
     }
     ArrayList<LandScape> getDataForViewPager(){
         ArrayList<LandScape> dsData = new ArrayList<>();
