d3f92c0524daa676b7b509061a7c7d16eb9b3659
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-04-16 07:08:35 +0700

    Bai Hoc Hom Nay
    up ++

--- ViewPager2RecylerViewAdapter/app/src/main/java/com/example/viewpager2recylerviewadapter/MainActivity.java
+++ ViewPager2RecylerViewAdapter/app/src/main/java/com/example/viewpager2recylerviewadapter/MainActivity.java
@@ -7,18 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> viewPagerDatas;
+    ViewPager2 viewPager2Land;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        viewPagerDatas = new getDataForViewPager();
+        viewPager2Land = findViewById(R.id.vp2Land);
+        landScapeAdapter = new LandScapeAdapter(this,viewPagerDatas,)
+    }
+    ArrayList<LandScape> getDataForViewPager(){
+        ArrayList<LandScape> dsData = new ArrayList<>();
+        LandScape landScape = new LandScape("des","Ảnh đẹp 4k");
+        dsData.add(landScape);
+        dsData.add(new LandScape("anh1","Ảnh đẹp 4k"));
+        dsData.add(new LandScape("anh2","Ảnh đẹp 4k"));
+        dsData.add(new LandScape("anh3","Ảnh đẹp 4k"));
+        return dsData;
     }
 }
